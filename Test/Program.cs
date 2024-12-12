015f011ce4f98edede63a31a19dec173db911512
YN <YN@DESKTOP-0KKVC4A>
2024-12-12 03:39:44 +0200

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,190 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Test
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            #region First Question
+            //Console.WriteLine("Enter Number");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 3 == 0 && num % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            #endregion
+
+            #region 2
+            //Console.WriteLine("Enter Number");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //if (num < 0)
+            //{
+            //    Console.WriteLine("Negative number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Positive number");
+            //}
+            #endregion
+
+            #region 3
+            //Console.WriteLine("Enter 3 Numbers");
+            //int mx = int.MinValue, mn = int.MaxValue;
+            //int num1 = int.Parse(Console.ReadLine()), num2 = int.Parse(Console.ReadLine()), num3 = int.Parse(Console.ReadLine());
+            //if (num1 > mx)
+            //{
+            //    mx = num1;
+            //}
+            //if (num2 > mx)
+            //{
+            //    mx = num2;
+            //}
+            //if (num3 > mx)
+            //{
+            //        mx = num3;
+            //}
+            //    if (num1 < mn)
+            //    {
+            //        mn = num1;
+            //    }
+            //    if (num2 < mn)
+            //    {
+            //        mn = num2;
+            //    }
+            //    if (num3 < mn)
+            //    {
+            //        mn = num3;
+            //    }
+            //    Console.WriteLine($"the Max value {mx} \n the Min value {mn}");
+            #endregion
+
+            #region 4
+            //Console.WriteLine("Enter Number");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("the Number is Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number Is ODD");
+            //}
+            #endregion
+
+            #region 5
+            //Console.WriteLine("Enter Character");
+            //char ch = char.Parse(Console.ReadLine());
+            //switch(ch)
+            //{
+            //    case 'a':
+            //    case 'o':
+            //    case 'e':
+            //    case 'u':
+            //    case 'i':
+            //        Console.WriteLine("Vowel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("consonant");
+            //        break;
+            //}
+            #endregion
+
+            #region 6
+            //Console.WriteLine("Enter Num");
+            //int num = int.Parse(Console.ReadLine());
+            //for(int i = 1; i < num; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region 7
+            //Console.WriteLine("Enter Num");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("multiplication table ");
+            //for (int i = 1; i < 13; i++)
+            //{
+            //    Console.WriteLine(i*num);
+            //}
+            #endregion
+
+            #region 8
+            //Console.WriteLine("Enter Num");
+            //int num = int.Parse(Console.ReadLine());
+            //for (int i = 2; i <= num; i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region 9
+            //Console.WriteLine("Enter 2 Numbers");
+            //int power = 1;
+            //int num = int.Parse(Console.ReadLine());
+            //int num1= int.Parse(Console.ReadLine());
+            //for (int i = 0; i <num1; i++)
+            //{
+            //    power *= num;
+            //}
+            //Console.WriteLine($"the power of {num} ^ {num1} : {power}");
+            #endregion
+
+            #region 10
+            //Console.WriteLine("Enter Marks of Five SubJects ");
+            //int avg = 0;
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    avg += int.Parse(Console.ReadLine());
+
+            //}
+            //Console.WriteLine(avg/5);
+
+            #endregion
+
+            #region 11
+            //Console.WriteLine("Enter month number ");
+            //int num = int.Parse(Console.ReadLine()); ;
+            //if (num == 1 || num == 3 || num == 5 || num == 7 || num == 8 || num == 10 || num == 12)
+            //{
+            //    Console.WriteLine("The number of Days : 31");
+            //}
+            //else if (num == 4 || num == 6 || num == 9 || num == 11)
+            //{
+            //    Console.WriteLine("The number of Days : 30");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number of Days : 28");
+
+            //}
+
+            #endregion
+
+            #region 12
+            //Console.WriteLine("Enter 2 numbers ");
+            //double num1 = int.Parse(Console.ReadLine()) ,num2= int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+            //Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            //Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            //Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+
+
+
+            #endregion
+
+
+        }
+    }
+}
+
+
